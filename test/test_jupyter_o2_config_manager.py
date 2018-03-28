a7f132cfa4a1b599e24b6ce8fbec8dd3666e7c40
Aaron Kollasch <awkollasch@gmail.com>
2018-03-28 12:17:38 -0400

    Fix test_jupyter_o2_config_manager.py

--- test/test_jupyter_o2_config_manager.py
+++ test/test_jupyter_o2_config_manager.py
@@ -1,11 +1,11 @@
 import unittest
 
-from jupyter_o2.config_manager import generate_config
+from jupyter_o2.config_manager import generate_config_file
 
 
 class TestConfigManager(unittest.TestCase):
     def test_generate_config(self):
         self.assertIsInstance(
-            generate_config("/tmp"),
+            generate_config_file("/tmp"),
             str
         )
