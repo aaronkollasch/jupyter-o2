0a1186c97ead2294820130ff5bf9539317454958
Lood <loodvn@gmail.com>
2022-03-04 17:46:37 -0500

    added some tests for config file and for pubkey auth

--- tests/test_jupyter_o2_config_manager.py
+++ tests/test_jupyter_o2_config_manager.py
@@ -1,5 +1,8 @@
 import os
-from jupyter_o2.config_manager import generate_config_file, CFG_FILENAME
+from configparser import ConfigParser
+
+from jupyter_o2.config_manager import generate_config_file, CFG_FILENAME, ConfigManager, JO2_DEFAULTS, \
+    CFG_SEARCH_LOCATIONS
 
 
 class TestConfigManager:
@@ -10,3 +13,24 @@ class TestConfigManager:
         """
         assert isinstance(generate_config_file(config_dir=tmpdir), str)
         assert os.path.exists(tmpdir / CFG_FILENAME)
+
+    def test_load_config_file(self, tmpdir, monkeypatch):
+        generate_config_file(config_dir=tmpdir)
+        monkeypatch.chdir(tmpdir)  # Temporarily change working dir, will change back after test
+
+        # Add a user to the config file by replacing the line
+        with open(tmpdir / CFG_FILENAME, 'r') as fp:
+            filedata = fp.read()
+
+        modified = filedata.replace("DEFAULT_USER =", "DEFAULT_USER = mock")
+        # print("tmp", modified)
+
+        with open(tmpdir / CFG_FILENAME, 'w') as fp:
+            fp.write(modified)
+
+        # load the config file
+        config_mgr = ConfigManager()
+        config = config_mgr.config
+
+        assert config.get("Defaults",
+                          "DEFAULT_USER") == "mock", f"Config file {tmpdir / CFG_FILENAME} not read in correctly"
